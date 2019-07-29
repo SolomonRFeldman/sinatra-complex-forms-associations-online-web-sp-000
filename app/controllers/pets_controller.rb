37e993c06d75625835bbbf90ed173863d8fc306f
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-29 15:30:30 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -28,7 +28,7 @@ class PetsController < ApplicationController
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
     if params[:owner][:name] != ""
-      @pet.owner_id = Owner.create(params[:owner]).id
+      @pet.owner_id = Owner.create(params[:owner][:name]).id
       @pet.save
     end
     binding.pry
