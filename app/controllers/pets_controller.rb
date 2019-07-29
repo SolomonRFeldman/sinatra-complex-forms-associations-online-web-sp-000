94f8d43d6c43606969e500ccefa882baeb92ab18
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-29 15:25:09 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -27,7 +27,7 @@ class PetsController < ApplicationController
   patch '/pets/:id' do 
     @pet = Pet.find(params[:id])
     @pet.update(params[:pet])
-    if !params[:owner].empty? Owner.create(params[:owner]).id
+    if !params[:owner].empty? 
       @pet.owner_id = owner_id
       @pet.save
     end
