1e5ff6a48caa0689e987c597fceeabc094a2655e
Solomon Feldman <solomonrfeldman@gmail.com>
2019-07-29 14:37:40 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -12,7 +12,7 @@ class PetsController < ApplicationController
 
   post '/pets' do 
     @pet = Pet.create(params[:pet])
-    @pet.owner = Owner.create(name: params["owner"]["name"]) unless @pet.owner = Owner.find(params[:owner_id])
+    @pet.owner = Owner.create(params[:owner]) unless @pet.owner = Owner.find(params[:owner_id])
     redirect to "pets/#{@pet.id}"
   end
 
